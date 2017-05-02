accc8628bc0ef80648c20f4692f64e376dd8dad5
cc <ch2en@outlook.com>
2017-05-02 20:56:17 +0800

    version 1.0 connot use

---
+++ sharemem.h
@@ -0,0 +1,30 @@
+#ifndef SHAREMEM_H
+#define SHAREMEM_H
+
+#define STATION 5
+
+class shareMemory{
+public:
+    int ticketLeft[STATION+1][STATION+1];
+
+    void init(){
+
+    }
+    void addTicket(int s,int t){
+
+    }
+
+    void buyTicket(int s,int t){
+
+    }
+}
+
+void createShareMem(){
+
+}
+void openShareMem(){
+    
+}
+
+
+#endif
